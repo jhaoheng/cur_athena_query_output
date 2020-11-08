975207d2f4807e204551f18866c3d372b5291be5
jhaoheng <jhaoheng@gmail.com>
2020-11-08 22:55:09 +0800

    add sns & update template

--- app/main.go
+++ app/main.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"os"
 	"strings"
 	"time"
 
@@ -14,12 +15,25 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/athena"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/sns"
 )
 
 var (
-	Database  = ""
-	Workgroup = ""
-	Querys    = []string{}
+	SNS_Topic_Arn                   = os.Getenv("SNS_Topic_Arn")
+	S3Bucket_Of_AthenaReport_Output = os.Getenv("S3Bucket_Of_AthenaReport_Output")
+	Athena_Database                 = os.Getenv("Athena_Database")
+	Athena_Workgroup                = os.Getenv("Athena_Workgroup")
+	Querys                          = []string{
+		`SELECT line_item_product_code,
+					sum(line_item_blended_cost) AS cost,
+					month
+			FROM all
+			WHERE year='2020'
+				AND month='11'
+			GROUP BY  line_item_product_code, month
+			HAVING sum(line_item_blended_cost) > 0
+			ORDER BY  line_item_product_code;`,
+	}
 )
 
 func handler(ctx context.Context, s3Event events.S3Event) error {
@@ -39,9 +53,18 @@ var mySession = session.Must(session.NewSession())
 var svc_athena = athena.New(mySession)
 
 func athena_startQueryExecution(query string) (queryExecutionId string) {
+	//
+	queryExecutionContext := athena.QueryExecutionContext{}
+	queryExecutionContext.SetDatabase(Athena_Database)
+	//
+	resultConfiguration := athena.ResultConfiguration{}
+	resultConfiguration.SetOutputLocation(S3Bucket_Of_AthenaReport_Output)
+	//
 	input := athena.StartQueryExecutionInput{
-		QueryString: aws.String(query),
-		WorkGroup:   aws.String(Workgroup),
+		QueryString:           aws.String(query),
+		WorkGroup:             aws.String(Athena_Workgroup),
+		QueryExecutionContext: &queryExecutionContext,
+		ResultConfiguration:   &resultConfiguration,
 	}
 	output, err := svc_athena.StartQueryExecution(&input)
 	if err != nil {
@@ -92,4 +115,20 @@ func s3_GetObjectAndPresignedURL(bucket, key string) (urlStr, contentStr string)
 		panic(err)
 	}
 	return urlStr, contentStr
+}
+
+//
+var svc_sns = sns.New(mySession)
+
+func sns_publish(message string) {
+	input := sns.PublishInput{}
+	input.SetMessage(message)
+	input.SetSubject("Cost And Usage Report")
+	input.SetTopicArn(SNS_Topic_Arn)
+
+	output, err := svc_sns.Publish(&input)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(output)
 }
